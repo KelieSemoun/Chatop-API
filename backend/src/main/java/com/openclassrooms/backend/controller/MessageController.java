c8290d792481783bc520f107a4b8bae7c104dfce
Kevin <kevinelie1998@gmail.com>
2025-02-11 15:49:27 +0100

    Added bad request response for messages + added swagger documentation on it

--- backend/src/main/java/com/openclassrooms/backend/controller/MessageController.java
+++ backend/src/main/java/com/openclassrooms/backend/controller/MessageController.java
@@ -6,10 +6,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.openclassrooms.backend.exception.ApiException;
 import com.openclassrooms.backend.model.ConfirmationDTO;
-import com.openclassrooms.backend.model.Message;
 import com.openclassrooms.backend.service.MessageService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
@@ -19,12 +24,37 @@ public class MessageController {
 	@Autowired
 	private MessageService messageService;
 	
+	@Operation(description = "Sends a message to a rental post")
+	@ApiResponses(value = {
+			@ApiResponse(
+				description = "Success",
+				responseCode = "200",
+				content = @Content(
+					schema = @Schema(implementation = ConfirmationDTO.class)
+				)
+			),
+			@ApiResponse(
+				description = "Unauthorized",
+				responseCode = "401",
+				content = @Content(
+					schema = @Schema(defaultValue = "Unauthorized")
+				)
+			),
+			@ApiResponse(
+				description = "Bad request due to either user not found or rental not found",
+				responseCode = "400",
+				content = @Content(
+					schema = @Schema(implementation = ApiException.class)
+				)
+			)
+	})
 	@PostMapping(path="/api/messages")
 	public ConfirmationDTO sendMessage(@RequestHeader("Authorization") String bearerToken,
 									   @RequestBody MessageDTO messageDTO) {
-		messageService.sendMessage(new Message(messageDTO.rental_id(),
-											   messageDTO.user_id(),
-											   messageDTO.message()));
+		
+		messageService.sendMessage(messageDTO.rental_id(),
+								   messageDTO.user_id(),
+								   messageDTO.message());
 		return new ConfirmationDTO("Message send with success");
 	}
 }
--- backend/src/main/java/com/openclassrooms/backend/service/MessageService.java
+++ backend/src/main/java/com/openclassrooms/backend/service/MessageService.java
@@ -1,10 +1,17 @@
 package com.openclassrooms.backend.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.openclassrooms.backend.exception.ApiRequestException;
 import com.openclassrooms.backend.model.Message;
+import com.openclassrooms.backend.model.Rental;
+import com.openclassrooms.backend.model.User;
 import com.openclassrooms.backend.repository.MessageRepository;
+import com.openclassrooms.backend.repository.RentalRepository;
+import com.openclassrooms.backend.repository.UserRepository;
 
 @Service
 public class MessageService {
@@ -12,8 +19,26 @@ public class MessageService {
 	@Autowired
 	private MessageRepository messageRepository;
 	
-	public void sendMessage(Message message) {
-		messageRepository.save(message);
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private RentalRepository rentalRepository;
+	
+	public void sendMessage(Integer rentalId, Integer userId, String message) {
+		Optional<User> sender = userRepository.findById(userId);
+		if(!sender.isPresent()) {
+			throw new ApiRequestException("User cannot be found !");
+		}
+		
+		Optional<Rental> rental = rentalRepository.findById(rentalId);
+		if(!rental.isPresent()) {
+			throw new ApiRequestException("Rental cannot be found !");
+		}
+		Message messageObj = new Message(rental.get().getId(),
+										 sender.get().getId(),
+										 message);
+		messageRepository.save(messageObj);
 	}
 
 }
